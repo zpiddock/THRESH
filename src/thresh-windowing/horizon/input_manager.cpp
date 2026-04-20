fa6d49b595ce9486d60506675e7ced36b6513175
Zach Piddock <zpiddock@gmail.com>
2026-04-20 20:31:30 +0100

    Add Mouse Handling to InputManager

--- src/thresh-windowing/horizon/input_manager.cpp
+++ src/thresh-windowing/horizon/input_manager.cpp
@@ -14,7 +14,7 @@ namespace horizon {
         m_last_key_state.assign(keycount, false);
     }
 
-    auto InputManager::is_key_down(const SDL_Scancode scancode) -> bool {
+    auto InputManager::is_key_held(const SDL_Scancode scancode) -> bool {
 
         return m_current_key_state[scancode];
     }
@@ -34,7 +34,33 @@ namespace horizon {
         return m_last_key_state[scancode];
     }
 
+    auto InputManager::is_mouse_button_held(uint32_t button) -> bool {
+
+        return m_current_mouse_state & SDL_BUTTON_MASK(button);
+    }
+
+    auto InputManager::mouse_button_just_pressed(uint32_t button) -> bool {
+
+        return (m_current_mouse_state & SDL_BUTTON_MASK(button)) && !(m_last_mouse_state & SDL_BUTTON_MASK(button));
+    }
+
+    auto InputManager::mouse_button_just_released(uint32_t button) -> bool {
+
+        return !(m_current_mouse_state & SDL_BUTTON_MASK(button)) && (m_last_mouse_state & SDL_BUTTON_MASK(button));
+    }
+
+    auto InputManager::get_mouse_state() -> std::pair<float, float> {
+        float x = 0.f;
+        float y = 0.f;
+        SDL_GetMouseState(&x, &y);
+        return {x, y};
+    }
+
     auto InputManager::flush_key_state() -> void {
         std::memcpy(m_last_key_state.data(), m_current_key_state, m_last_key_state.size() * sizeof(bool));
+        m_last_mouse_state = m_current_mouse_state;
+        m_current_mouse_state = SDL_GetMouseState(nullptr, nullptr);
+
+        SDL_GetRelativeMouseState(&m_mouse_dx, &m_mouse_dy);
     }
 } // horizon
--- src/thresh-windowing/horizon/input_manager.hpp
+++ src/thresh-windowing/horizon/input_manager.hpp
@@ -6,6 +6,7 @@
 #include <vector>
 
 #include "SDL3/SDL_keyboard.h"
+#include "SDL3/SDL_mouse.h"
 #include "SDL3/SDL_scancode.h"
 
 namespace horizon {
@@ -15,7 +16,7 @@ class InputManager {
     public:
         auto init() -> void;
 
-        auto is_key_down(SDL_Scancode scancode) -> bool;
+        auto is_key_held(SDL_Scancode scancode) -> bool;
 
         auto key_just_pressed(SDL_Scancode scancode) -> bool;
 
@@ -23,11 +24,24 @@ class InputManager {
 
         auto was_key_pressed_last_frame(SDL_Scancode scancode) -> bool;
 
+        auto is_mouse_button_held(uint32_t button) -> bool;
+
+        auto mouse_button_just_pressed(uint32_t button) -> bool;
+
+        auto mouse_button_just_released(uint32_t button) -> bool;
+
+        auto get_mouse_state() -> std::pair<float, float>;
+
         auto flush_key_state() -> void;
     private:
         const bool*       m_current_key_state = nullptr;
         std::vector<uint8_t> m_last_key_state    = {};
 
+        uint32_t m_current_mouse_state = 0;
+        uint32_t m_last_mouse_state = 0;
+
+        float m_mouse_dx = 0.0f;
+        float m_mouse_dy = 0.0f;
 };
 
 } // horizon
