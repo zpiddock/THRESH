7b953cc5e9849cc5a9ac9aae06c2032892766a63
Zach Piddock <zpiddock@gmail.com>
2026-05-17 17:31:14 +0100

    Toggleable movement tied to imgui overlay

--- examples/demo-app/game_app.cpp
+++ examples/demo-app/game_app.cpp
@@ -16,7 +16,7 @@ namespace demo {
         SUB_INFO("Starting Demo Game");
         auto scene = std::make_unique<thresh::Scene>();
 
-        auto player = scene->create_entity()
+        auto player = scene->get_or_create_entity("Player")
         .set<Transform>({.position = {0.0f, 1.0f, 5.0f}})
         .set<Camera>({})
         .set<CameraController>({})
@@ -27,8 +27,8 @@ namespace demo {
         auto box_material = thresh::Engine::get_instance().graphics()->register_material(box_texture);
         auto red_material = thresh::Engine::get_instance().graphics()->register_material(box_texture, {1.0f, 0.0f, 0.0f, 1.0f});
 
-        scene->create_entity("Test Cube").set<Transform>({.position = {0.0f, 0.0f, 0.0f}}).set<Mesh>({.handle = box_mesh, .material_handle = box_material});
-        scene->create_entity("Test Cube 2").set<Transform>({.position = {2.0f, 0.0f, 0.0f}}).set<Mesh>({.handle = box_mesh, .material_handle = red_material});
+        scene->get_or_create_entity("Test Cube").set<Transform>({.position = {0.0f, 0.0f, 0.0f}}).set<Mesh>({.handle = box_mesh, .material_handle = box_material});
+        scene->get_or_create_entity("Test Cube 2").set<Transform>({.position = {2.0f, 0.0f, 0.0f}}).set<Mesh>({.handle = box_mesh, .material_handle = red_material});
 
         thresh::Engine::get_instance().transition_scene(std::move(scene));
     }
@@ -38,6 +38,13 @@ namespace demo {
         auto* input = thresh::Engine::get_instance().input();
         if (input->key_just_released(SDL_SCANCODE_ESCAPE)) {
             thresh::Engine::get_instance().window()->setShouldClose(true);
+        }
+        if (input->key_just_pressed(SDL_SCANCODE_F1)) {
+            auto imgui_enabled = thresh::Engine::get_instance().graphics()->is_imgui_enabled();
+            thresh::Engine::get_instance().graphics()->imgui_enabled(!imgui_enabled);
+            auto scene = thresh::Engine::get_instance().active_scene();
+            auto player = scene->get_or_create_entity("Player");
+            player.get_mut<CameraController>().movement_allowed = imgui_enabled;
         }
     }
 
