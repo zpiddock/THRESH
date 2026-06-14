46040a5afc63f9d6fd46ab5d7ff17d0a8701d4d7
Zach Piddock <zpiddock@gmail.com>
2026-06-14 19:18:17 +0100

    Added new frame context and gpu data structures,

---
+++ src/thresh-rendering/flux/vkbackend/frame_context.cpp
@@ -0,0 +1,37 @@
+//
+// Created by Admin on 14/06/2026.
+//
+
+#include "frame_context.hpp"
+
+#include "vulkan_device.hpp"
+
+namespace flux {
+    auto FrameContext::create(ThreshVkDevice& device) -> FrameContext {
+
+        FrameContext frame;
+
+        frame.uniforms = Buffer(device, {
+            .size = LIGHT_DATA_OFFSET + sizeof(flux::gpu::LightData),
+            .usage = vk::BufferUsageFlagBits::eShaderDeviceAddress,
+            .memory = vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent,
+            .persistent_map = true,
+            .debug_name = "Frame Uniforms"
+        });
+        frame.camera_address = frame.uniforms.device_address() + CAMERA_OFFSET;
+        frame.light_data_address = frame.uniforms.device_address() + LIGHT_DATA_OFFSET;
+
+        // Persistent primary command buffer
+        const vk::CommandBufferAllocateInfo alloc_info{
+            .commandPool = device.command_pool(),
+            .level       = vk::CommandBufferLevel::ePrimary,
+            .commandBufferCount = 1
+        };
+        frame.command_buffer = flux::CommandBuffer{std::move(device.logical().allocateCommandBuffers(alloc_info).front())};
+
+        frame.present_complete = vk::raii::Semaphore(device.logical(), vk::SemaphoreCreateInfo{});
+        frame.in_flight_fence  = vk::raii::Fence(device.logical(), vk::FenceCreateInfo{.flags = vk::FenceCreateFlagBits::eSignaled});
+
+        return frame;
+    }
+} // flux
---
+++ src/thresh-rendering/flux/vkbackend/frame_context.hpp
@@ -0,0 +1,30 @@
+//
+// Created by Admin on 14/06/2026.
+//
+
+#pragma once
+#include <vulkan/vulkan.hpp>
+
+#include "buffer.hpp"
+#include "command_buffer.hpp"
+#include "flux/gpu_data.hpp"
+
+namespace flux {
+    class ThreshVkDevice;
+
+    struct FrameContext {
+
+        static constexpr vk::DeviceSize CAMERA_OFFSET = 0;
+        static constexpr vk::DeviceSize LIGHT_DATA_OFFSET = (sizeof(flux::gpu::CameraData) + 15) & ~vk::DeviceSize{15};
+
+        flux::Buffer uniforms;
+        vk::DeviceAddress camera_address;
+        vk::DeviceAddress light_data_address;
+
+        CommandBuffer command_buffer;
+        vk::raii::Semaphore present_complete = nullptr;
+        vk::raii::Fence in_flight_fence = nullptr;
+
+        static auto create(ThreshVkDevice& device) -> FrameContext;
+    };
+} // flux
