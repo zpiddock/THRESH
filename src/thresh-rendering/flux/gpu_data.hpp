3479b18f11f3f0a2a188a72bd09df2a0fa0904c5
Zach Piddock <zpiddock@gmail.com>
2026-06-20 11:12:52 +0100

    Added MaterialData structs

--- src/thresh-rendering/flux/gpu_data.hpp
+++ src/thresh-rendering/flux/gpu_data.hpp
@@ -11,6 +11,11 @@ namespace flux::gpu {
 
     inline constexpr auto MAX_POINT_LIGHTS = 4;
 
+    inline constexpr std::uint32_t MATERIAL_FLAG_ALPHA_MASK   = 1u << 0;
+    inline constexpr std::uint32_t MATERIAL_FLAG_DOUBLE_SIDED = 1u << 1;
+    inline constexpr std::uint32_t MATERIAL_FLAG_HAS_NORMAL   = 1u << 2;
+    inline constexpr std::uint32_t MATERIAL_FLAG_HAS_EMISSIVE = 1u << 3;
+
     struct alignas(16) CameraData {
 
         flux::float4x4 view;
@@ -46,4 +51,22 @@ namespace flux::gpu {
         DescriptorHandle  sampler;
     };
     static_assert(sizeof(CompositePushConstants) == 16);
+
+    struct alignas(16) MaterialData {
+
+        flux::float4  base_colour_factor;
+        flux::float3  emissive_factor;
+        float         metallic_factor;
+        float         roughness_factor;
+        float         normal_scale;
+        float         occlusion_strength;
+        float         alpha_cutoff;
+        std::uint32_t base_colour_texture_handle;
+        std::uint32_t normal_texture_handle;
+        std::uint32_t emissive_texture_handle;
+        std::uint32_t metallic_roughness_texture_handle; // G Chan: Roughness, B Chan: Metallic
+        std::uint32_t occlusion_texture_handle;
+        std::uint32_t flags;
+    };
+    static_assert(sizeof(MaterialData) == 80);
 }
